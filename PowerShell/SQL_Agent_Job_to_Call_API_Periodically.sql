bfa034a55588b2fcf82db93312c43ec69a67464a
Ahad Porkar <47736575+AhadPorkar@users.noreply.github.com>
2025-12-11 10:40:22 +0330

    Update SQL_Agent_Job_to_Call_API_Periodically.sql

--- PowerShell/SQL_Agent_Job_to_Call_API_Periodically.sql
+++ PowerShell/SQL_Agent_Job_to_Call_API_Periodically.sql
@@ -1,3 +1,11 @@
+-- Enable xp_cmdshell (requires sysadmin privileges)
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+EXEC sp_configure 'xp_cmdshell', 1;
+RECONFIGURE;
+GO
+
+
 -- Create PowerShell script as a SQL Agent Job Step
 USE msdb;
 GO
